a2b31d52a87cea9dcae420167a7b29d62c948718
Ecenur <ece_nurum_37@hotmail.com>
2022-01-21 21:13:59 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    static int usbulma(int sayi, int us){
+        if(us < 1){
+            return 1;
+        }
+        else{
+            return sayi * usbulma(sayi,us-1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int sayi, us;
+
+        System.out.print("Taban giriniz: ");
+        sayi = inp.nextInt();
+        System.out.print("Üs giriniz: ");
+        us = inp.nextInt();
+
+        System.out.println("Sonuc:" +usbulma(sayi,us));
+    }
+}
